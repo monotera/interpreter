120828d6244236d1fefca6a5cf4199a8f7981f4d
CarlosAndresErazoGarzon <carlos-erazo@javeriana.edu.co>
2021-05-21 22:31:28 -0500

    Deja version lista para la entrega final

---
+++ interpreter/src/main/java/com/nexler/nx/interpreter/ast/Multiplication.java
@@ -0,0 +1,24 @@
+package com.nexler.nx.interpreter.ast;
+
+import java.util.Map;
+
+public class Multiplication implements ASTNode {
+	private ASTNode operand1;
+	private ASTNode operand2;
+	
+	
+	
+	public Multiplication(ASTNode operand1, ASTNode operand2) {
+		super();
+		this.operand1 = operand1;
+		this.operand2 = operand2;
+	}
+
+
+
+	@Override
+	public Object execute(Map<String, Object> symbolTable) {
+		return (int)operand1.execute(symbolTable)*(int)operand2.execute(symbolTable);
+	}
+
+}
